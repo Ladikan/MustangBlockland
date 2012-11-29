fffc3f5045e515f39e274c2950688e7adff26594
Ladikan <mladikan@gmail.com>
2012-11-28 20:10:24 -0500

    Update Script_MustBot/testing.cs

--- Script_MustBot/testing.cs
+++ Script_MustBot/testing.cs
@@ -1,11 +1,1 @@
-function SimObject::raiseToThePower(%this, %power)
-{
-	for(%i = 0; %i < %power; %i++)
-	{
-		%answer = %this*%this
-	}
-	echo(%this@" to the "@%power@" power: "@%answer@".");
-}
-
-$obj = new SimObject();
-$obj.raiseToThePower(5);
+//Do nothing as of now...
